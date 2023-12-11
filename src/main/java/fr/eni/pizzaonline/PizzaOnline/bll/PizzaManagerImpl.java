a6bb6a0853b050c497fb6fe26891bb3ba62d89a5
Daniele Gioria <daniele.gioria2023@campus-eni.fr>
2023-12-11 13:45:19 +0100

    Added bll (no dal)

---
+++ src/main/java/fr/eni/pizzaonline/PizzaOnline/bll/PizzaManagerImpl.java
@@ -0,0 +1,21 @@
+package fr.eni.pizzaonline.PizzaOnline.bll;
+
+import java.util.List;
+
+import fr.eni.pizzaonline.PizzaOnline.bo.Pizza;
+
+public class PizzaManagerImpl implements PizzaManager{
+
+	@Override
+	public void addPizza(Pizza voiture) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Pizza> getAllPizzas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
