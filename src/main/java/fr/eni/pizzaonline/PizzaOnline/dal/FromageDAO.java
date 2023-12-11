b2cdd404e121fd1ef3d0627d87e27123db219a42
davidel29 <125995127+davidel29@users.noreply.github.com>
2023-12-11 14:33:11 +0100

    refonte arborescence

---
+++ src/main/java/fr/eni/pizzaonline/PizzaOnline/dal/FromageDAO.java
@@ -0,0 +1,7 @@
+package fr.eni.pizzaonline.PizzaOnline.dal;
+
+import fr.eni.pizzaonline.PizzaOnline.bo.Fromage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FromageDAO extends CrudRepository<Fromage, Integer> {
+}
