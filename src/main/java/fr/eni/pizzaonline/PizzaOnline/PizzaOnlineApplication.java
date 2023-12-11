758417566cb226ea247474d94314b4f51ca44fef
Daniele Gioria <daniele.gioria2023@campus-eni.fr>
2023-12-11 11:58:36 +0100

    second commit

---
+++ src/main/java/fr/eni/pizzaonline/PizzaOnline/PizzaOnlineApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.pizzaonline.PizzaOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOnlineApplication.class, args);
+	}
+
+}
