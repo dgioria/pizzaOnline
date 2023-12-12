03602d91c8bee5d93c2da93dd4208feb9106c0a8
davidel29 <125995127+davidel29@users.noreply.github.com>
2023-12-12 22:54:20 +0100

    inscription et connexion

--- src/main/java/fr/eni/pizzaonline/PizzaOnline/bll/PizzaManager.java
+++ src/main/java/fr/eni/pizzaonline/PizzaOnline/bll/PizzaManager.java
@@ -5,7 +5,7 @@ import fr.eni.pizzaonline.PizzaOnline.bo.Pizza;
 import java.util.List;
 
 public interface PizzaManager {
-    public void addPizza(Pizza voiture);
+    public void addPizza(Pizza pizza);
 
     public List<Pizza> getAllPizzas();
 }
