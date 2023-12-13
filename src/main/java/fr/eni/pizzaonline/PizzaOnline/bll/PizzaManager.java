1aaa68bae268a8c6270d12a4653009bc10558a99
davidel29 <125995127+davidel29@users.noreply.github.com>
2023-12-13 22:19:03 +0100

    restaure PizzaManager

--- src/main/java/fr/eni/pizzaonline/PizzaOnline/bll/PizzaManager.java
+++ src/main/java/fr/eni/pizzaonline/PizzaOnline/bll/PizzaManager.java
@@ -1,15 +1,13 @@
 package fr.eni.pizzaonline.PizzaOnline.bll;
 
-import fr.eni.pizzaonline.PizzaOnline.bo.CommandeLigne;
+import fr.eni.pizzaonline.PizzaOnline.bo.OrderRow;
 import fr.eni.pizzaonline.PizzaOnline.bo.Pizza;
-import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 public interface PizzaManager {
-    @Transactional
     public void addPizza(Pizza pizza);
 
     public List<Pizza> getAllPizzas();
-    public Double computFinalPrice(List<CommandeLigne> order);
+    public Double computFinalPrice(List<OrderRow> order);
 }
